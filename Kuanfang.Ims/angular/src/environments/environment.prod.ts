ee2ea5fafcb0a551a239b1257da735e24ebc7550
魏丹 <471309636@qq.com>
2021-01-20 18:56:26 +0800

    提交

--- Kuanfang.Ims/angular/src/environments/environment.prod.ts
+++ Kuanfang.Ims/angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://192.168.31.160:4200';
+const baseUrl = 'http://localhost:4200';
 
 export const environment = {
   production: true,
@@ -10,15 +10,16 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'http://192.168.31.160:44395',
+    issuer: 'http://s239r19764.wicp.vip:23180',
     redirectUri: baseUrl,
     clientId: 'Ims_App',
     responseType: 'code',
     scope: 'offline_access Ims',
+    requireHttps:false,
   },
   apis: {
     default: {
-      url: 'http://192.168.31.160:44395',
+      url: 'http://s239r19764.wicp.vip:23180',
       rootNamespace: 'Kuanfang.Ims',
     },
   },
